853031a3dbcd167d34541760ef11aa748e03b5a4
trungdt712 <s3443450@student.rmit.edu.au>
2015-05-21 15:36:31 +1000

    fix menu bar
    menubar fix

--- includes/menu.php
+++ includes/menu.php
@@ -1,7 +1,7 @@
 <!-- Nav bar --> 
 	<div class="nav">
-		<div><a href="index.html">CINEMA</a></div>
-		<div><a href="movies.html">MOVIE</a></div>
-		<div><a href="price.html">PRICING</a></div>
-		<div><a href="contact.html">CONTACT US</a></div>
+		<div><a href="index.php">CINEMA</a></div>
+		<div><a href="movies.php">MOVIE</a></div>
+		<div><a href="price.php">PRICING</a></div>
+		<div><a href="contact.php">CONTACT US</a></div>
 	</div> 
